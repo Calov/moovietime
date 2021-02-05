15d8ec5a9cae7168d564e2b2153c6d2e59f01f02
Calov <kevincalov@gmail.com>
2021-02-05 17:17:14 +0700

    add button component and navbar category

---
+++ components/__ui/button/UiButton.vue
@@ -0,0 +1,115 @@
+<template>
+  <button
+    :class="styleClass"
+    :disabled="isDisabled"
+    :type="buttonType"
+    @click="$emit('click')"
+  >
+    <slot />
+  </button>
+</template>
+
+<script>
+  export default {
+    name: 'UiButton',
+
+    props: {
+      isWhite: { type: Boolean, default: false },
+      isBlock: { type: Boolean, default: false },
+      isMedium: { type: Boolean, default: false },
+      isDisabled: { type: Boolean, default: false },
+      isOutlined: { type: Boolean, default: false },
+      isSmall: { type: Boolean, default: false },
+      isRounded: { type: Boolean, default: false },
+      buttonType: { type: String, default: 'button' }
+    },
+
+    computed: {
+      styleClass() {
+        return [
+          'ui-button',
+          {
+            'ui-button--is-white': this.isWhite,
+            'ui-button--is-block': this.isBlock,
+            'ui-button--is-medium': this.isMedium,
+            'ui-button--is-outlined': this.isOutlined,
+            'ui-button--is-small': this.isSmall,
+            'ui-button--is-rounded': this.isRounded
+          }
+        ]
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  .ui-button {
+    display: inline-block !important;
+    border: none;
+    padding: 0;
+    cursor: pointer;
+    background-color: #1da1f2;
+    border-radius: 5px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 5px 20px;
+    min-height: 38px;
+    color: #000000;
+    font-size: 1.5em;
+    transition: background-color 0.2s ease, border-color 0.2s ease;
+
+    &:hover {
+      background-color: darken(#1da1f2, 2.5%);
+      border-color: darken(#1da1f2, 2.5%);
+    }
+
+    &--is-block {
+      width: 100%;
+      display: block;
+    }
+
+    &--is-medium {
+      font-size: 14px;
+      min-height: 30px;
+    }
+
+    &--is-small {
+      font-size: 12px;
+      min-height: 24px;
+    }
+
+    &--is-white {
+      background-color: #ffffff;
+      border-color: #ffffff;
+      color: #ffffff;
+
+      &:hover {
+        color: #1da1f2;
+        background-color: #ffffff;
+      }
+    }
+
+    &--is-outlined {
+      background-color: transparent;
+      border-width: 1.5px;
+      border-style: solid;
+    }
+
+    &--is-rounded {
+      border-radius: 20px;
+    }
+
+    &[disabled] {
+      background-color: #ddd;
+      border-color: #ddd;
+      color: #777;
+      cursor: not-allowed;
+    }
+
+    &:active,
+    &:focus {
+      outline: none;
+    }
+  }
+</style>
---
+++ components/__ui/navbar/UiNavbarCategory.vue
@@ -0,0 +1,90 @@
+<template>
+  <div v-if="isShow" class="ui-navbar-category">
+    <div class="ui-navbar-category__triangle"></div>
+    <ul>
+      <li>
+        <a href="#">Action</a>
+      </li>
+      <li>
+        <a href="#">Horror</a>
+      </li>
+      <li>
+        <a href="#">Commedy</a>
+      </li>
+      <li>
+        <a href="#">Drama</a>
+      </li>
+      <li>
+        <a href="#">Romantic</a>
+      </li>
+      <li>
+        <a href="#">Colosal</a>
+      </li>
+      <li>
+        <a href="#">Thriller</a>
+      </li>
+      <li>
+        <a href="#">Adventure</a>
+      </li>
+      <li>
+        <a href="#">Fantasy</a>
+      </li>
+      <li>
+        <a href="#">Animation</a>
+      </li>
+      <li>
+        <a href="#">Family</a>
+      </li>
+      <li>
+        <a href="#">Others</a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      isShow: {
+        type: Boolean,
+        default: false
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  .ui-navbar-category {
+    background-color: #1da1f2;
+    position: absolute;
+    top: 125px;
+    height: 75px;
+    width: 100%;
+    left: 0;
+    display: flex;
+    align-items: center;
+    ul {
+      list-style: none;
+      display: flex;
+      justify-content: space-evenly;
+      padding: 0;
+      width: 100%;
+      &::before {
+        content: '\25B2';
+        color: #1da1f2;
+        position: absolute;
+        top: -9px;
+        right: calc(28%);
+        font-size: 10px;
+        transform: scaleX(3);
+      }
+      li {
+        a {
+          text-decoration: none;
+          color: #fff;
+          font-size: 24px;
+        }
+      }
+    }
+  }
+</style>
