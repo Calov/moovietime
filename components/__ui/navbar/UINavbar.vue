2803184c09988d012cafe8b199832fad88be32d2
Calov <kevincalov@gmail.com>
2021-02-05 17:14:14 +0700

    add navbar layout and brand

---
+++ components/__ui/navbar/UINavbar.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="navbar">
+    <div class="navbar__container">
+      <UiNavbarBrand />
+      <UiSearch />
+      <a class="navbar__container__item" @click="categoryShow = !categoryShow">
+        Category
+      </a>
+      <NuxtLink class="navbar__container__item" to="/"> Login </NuxtLink>
+      <UiButton :is-outlined="true" :is-white="true">Get Started</UiButton>
+      <UiNavbarCategory :is-show="categoryShow" />
+    </div>
+  </div>
+</template>
+
+<script>
+  import UiButton from '@/components/__ui/button/UiButton'
+  import UiSearch from '@/components/__ui/search/UiSearch'
+  import UiNavbarBrand from './UiNavbarBrand'
+  import UiNavbarCategory from './UiNavbarCategory'
+
+  export default {
+    components: {
+      UiNavbarBrand,
+      UiNavbarCategory,
+      UiButton,
+      UiSearch
+    },
+    data() {
+      return {
+        categoryShow: false
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
+  .navbar {
+    min-height: 12.5vh;
+    width: 100%;
+    background-color: #1da1f2;
+    padding: 2vw;
+    &__container {
+      width: 100%;
+      color: #ffffff;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+
+      &__item {
+        color: #ffffff;
+        text-decoration: none;
+        font-size: 1.75em;
+        flex-grow: 1;
+        text-align: center;
+        cursor: pointer;
+      }
+    }
+  }
+</style>
---
+++ components/__ui/navbar/UiNavbarBrand.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="ui-navbar-brand">
+    <NuxtLink class="ui-navbar-brand__logo" to="/">
+      <img :src="logo" alt="Moovietime" />
+    </NuxtLink>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'NavbarBrand',
+
+    data() {
+      return {
+        logo: require('./logo.png')
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  .ui-navbar-brand {
+    padding-top: 0.5em;
+    &__logo {
+      &:hover {
+        img {
+          transform: scale(1.025);
+        }
+      }
+
+      img {
+        max-height: 50px;
+        transition: transform 0.2s ease-in-out;
+      }
+    }
+  }
+</style>
